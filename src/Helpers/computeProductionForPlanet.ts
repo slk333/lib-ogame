5b0254f42a5122957125ab906997b7ea053d7ac0
Antoine Weber <aweb33@icloud.com>
2024-09-06 15:42:03 +0200

    small refactor

--- src/Helpers/computeProductionForPlanet.ts
+++ src/Helpers/computeProductionForPlanet.ts
@@ -5,25 +5,28 @@ import {
 } from "../Production/production"
 import { FirestorePlanet } from "../Types/FirestorePlanet"
 
-export function computeProductionForPlanet(firestorePlanet: FirestorePlanet) {
-    const planetDefaultProduction = {
+type ResourcesProduction_s = {
+    metalProduction_s: number
+    crystalProduction_s: number
+    deuteriumProduction_s: number
+}
+
+export function computeProductionForPlanet(planet: FirestorePlanet): ResourcesProduction_s {
+    /* 0.1 native production */
+    const nativeProduction_s = {
         metalProduction_s: 30 / 3600,
         crystalProduction_s: 15 / 3600,
         deuteriumProduction_s: 0,
     }
-    const metalMineProduction_s = getMetalMineProduction_s(
-        firestorePlanet.structures.metalMine.level
-    )
-    const crystalMineProduction_s = getCrystalMineProduction_s(
-        firestorePlanet.structures.crystalMine.level
-    )
+    /* 0.2 mines production */
+    const metalMineProduction_s = getMetalMineProduction_s(planet.structures.metalMine.level)
+    const crystalMineProduction_s = getCrystalMineProduction_s(planet.structures.crystalMine.level)
     const deuteriumMineProduction_s = getDeuteriumProduction_s(
-        firestorePlanet.structures.deuteriumSynthesizer.level
+        planet.structures.deuteriumSynthesizer.level
     )
-
-    const metalProduction_s = metalMineProduction_s + planetDefaultProduction.metalProduction_s
-    const crystalProduction_s =
-        crystalMineProduction_s + planetDefaultProduction.crystalProduction_s
+    /* 0.3 native + mines production */
+    const metalProduction_s = metalMineProduction_s + nativeProduction_s.metalProduction_s
+    const crystalProduction_s = crystalMineProduction_s + nativeProduction_s.crystalProduction_s
     const deuteriumProduction_s = deuteriumMineProduction_s
 
     return { metalProduction_s, crystalProduction_s, deuteriumProduction_s }
--- src/Helpers/computeStructureUpgradeTimeForPlanet.ts
+++ src/Helpers/computeStructureUpgradeTimeForPlanet.ts
@@ -3,12 +3,13 @@ import { FirestorePlanet } from "../Types/FirestorePlanet"
 import { StructureName } from "../Types/StructureName"
 
 export function computeStructureUpgradeTimeForPlanet(
-    firestorePlanet: FirestorePlanet,
+    planet: FirestorePlanet,
     structureName: StructureName
 ): number {
-    const targetLevel = firestorePlanet.structures[structureName].level + 1
-    const roboticsFactoryLevel = firestorePlanet.structures["roboticsFactory"].level
-    const naniteFactoryLevel = firestorePlanet.structures["naniteFactory"].level
+    const targetLevel = planet.structures[structureName].level + 1
+    const roboticsFactoryLevel = planet.structures["roboticsFactory"].level
+    const naniteFactoryLevel = planet.structures["naniteFactory"].level
+
     const upgradeTime_s = getStructureConstructionTime(
         structureName,
         targetLevel,
