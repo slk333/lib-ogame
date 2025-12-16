ea93997d5c7535703fdecbecbdfc5c1908d8f0a3
slk333 <aweb33@icloud.com>
2025-12-16 17:41:38 +0100

    2.4.0

--- src/Types/Planet.ts
+++ src/Types/Planet.ts
@@ -16,6 +16,7 @@ export interface Planet {
 
     createdAt?: string // ISO
     coordinates: Coordinates
+    temperature: number
 
     structures: Record<StructureName, PlanetStructure>
     ships: Record<ShipName, { name: ShipName; count: number }>
