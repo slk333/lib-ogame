7311e7342d7ce2d34d5f27dfb29f97d0e84cc523
Antoine Weber <aweb33@icloud.com>
2025-12-18 21:44:46 +0100

    2.4.4: Message is now a type union over more specific ones such as SpyReportMessage

--- src/Types/Message.ts
+++ src/Types/Message.ts
@@ -1,6 +1,7 @@
-import { PublicPlayer } from "./PublicPlayer.js"
+import type { Planet, ResourcesRecord } from "../index.js"
+import type { PublicPlayer } from "./PublicPlayer.js"
 
-export interface Message {
+export interface BaseMessage {
     readonly id: string
     readonly emittedAt: string
 
@@ -8,9 +9,28 @@ export interface Message {
     readonly recipient: PublicPlayer
 
     readonly status: "unread" | "read"
-    readonly content: string
+    readonly summary: string
 
     readonly expireAt: Date
     // timestamp for firestore periodic deletion, set to return date + 24hours
     // set type to Date so it's stored as Timestamp in the DB
 }
+
+export type SpyReportPlanetData = Pick<Planet, "name" | "coordinates" | "structures" | "ships"> & {
+    resources: ResourcesRecord // current resources at the time of the report
+}
+
+export interface SpyReportMessage extends BaseMessage {
+    readonly type: "spyReport"
+    readonly planetData: SpyReportPlanetData
+}
+export interface PlayerMessage extends BaseMessage {
+    readonly type: "playerMessage"
+    readonly fullMessage: string
+}
+export interface SystemMessage extends BaseMessage {
+    readonly type: "systemMessage"
+    readonly fullMessage: string
+}
+
+export type Message = SpyReportMessage | PlayerMessage | SystemMessage
